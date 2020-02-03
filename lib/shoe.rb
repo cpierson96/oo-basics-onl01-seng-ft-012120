1d4d05022a738295d248ac7d8166942dd70710cb
Claudia Pierson <c172@umbc.edu>
2020-02-03 06:22:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,10 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :cobble
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
     def initialize(brand)
       @brand = brand
+      def shoe_cobble
+        puts "Your shoe is as good as new!"
     end
   
 end
