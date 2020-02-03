627b3c0ed5a634c374cc75ae746e5477cab8dbf9
Claudia Pierson <c172@umbc.edu>
2020-02-03 06:26:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition ="new"
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-    def initialize(brand)
+    def initialize(brand, shoe_condition="new")
       @brand = brand
     end
       def cobble
